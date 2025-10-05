e3c532ff4176d42c01bca72df92241ca8f085bb6
pengqianhan <hanpengqian@outlook.com>
2025-10-06 11:17:46 +1300

    Refactor basic usage script for harmonic analysis: update data loading, enhance image generation, and improve output handling

--- basic_usage_ha.py
+++ basic_usage_ha.py
@@ -16,30 +16,31 @@ client = openai.OpenAI(
 # model_name = "moonshotai/kimi-vl-a3b-thinking:free" # not working
 model_name = "qwen/qwen2.5-vl-32b-instruct:free"
 # Generate data
-x = np.linspace(-1, 1, 50)
-y = np.sin(np.pi * x) + 0.1 * np.random.randn(50)
+# x = np.linspace(-1, 1, 50)
+# y = np.sin(np.pi * x) + 0.1 * np.random.randn(50)
 
-# Generate image of data (or use your own)
-fig, ax = plt.subplots()
-ax.scatter(x, y)
-base64_img = llmlex.images.generate_base64_image(fig, ax, x, y)
+# # Generate image of data (or use your own)
+# fig, ax = plt.subplots()
+# ax.scatter(x, y)
+# base64_img = llmlex.images.generate_base64_image(fig, ax, x, y)
 
 npz_file = np.load('data_duffing/test_data0.npz')
-state_data = npz_file['state_data']
-input_data = npz_file['input_data']
+state_data = npz_file['state']
+input_data = npz_file['input']
 dt=0.001
 cnt = 0
 system_name = "Duffing Oscillator"
-base64_img_ha = llmlex.images.generate_base64_image_ha(state_data, input_data, dt, system_name,cnt,None,False)
+base64_img_ha = llmlex.images.generate_base64_image_ha(state_data, input_data, dt, system_name,cnt,None)
+
 
 # Run symbolic regression
-result = llmlex.single_call(client, base64_img, x, y, model=model_name)
+# result = llmlex.single_call(client, base64_img, x, y, model=model_name)
 result_ha = llmlex.single_call(client, base64_img_ha, state_data, input_data, model=model_name)
 
 # View results
-print(f"Best function: {result['ansatz']}")
-print(f"Parameters: {result['params']}")
-print(f"Score: {result['score']}")
+print(f"Best function: {result_ha['ansatz']}")
+print(f"Parameters: {result_ha['params']}")
+print(f"Score: {result_ha['score']}")
 
 # For more complex problems, use genetic algorithm approach
 # populations = llmlex.run_genetic(
