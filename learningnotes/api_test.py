4c566825462be0181e4de07bcfe93830145020a0
pengqianhan <hanpengqian@outlook.com>
2025-10-10 14:16:09 +1300

    Update API test script to switch from OpenRouter to Gemini integration, modifying client setup and model reference

--- learningnotes/api_test.py
+++ learningnotes/api_test.py
@@ -6,13 +6,21 @@ import matplotlib.pyplot as plt
 import llmlex
 load_dotenv() # 加载.env文件中的环境变量
 
-# Set up API client
-client = openai.OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY") if os.getenv("OPENROUTER_API_KEY") else "<<<<<<your_api_key>>>>>>>", 
-)
+# Set up API client for openrouter
+# client = openai.OpenAI(
+#     base_url="https://openrouter.ai/api/v1",
+#     api_key=os.getenv("OPENROUTER_API_KEY") if os.getenv("OPENROUTER_API_KEY") else "<<<<<<your_api_key>>>>>>>", 
+# )
+# model = "qwen/qwen2.5-vl-32b-instruct:free"
 
-model = "qwen/qwen2.5-vl-32b-instruct:free"
+# Set up API client for gemini
+client = openai.OpenAI(
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    api_key=os.getenv("GEMINI_API_KEY") if os.getenv("GEMINI_API_KEY") else "<<<<<<your_api_key>>>>>>>", 
+)
+model = "models/gemini-flash-lite-latest"
+
+print("Model: ", model)
 system_prompt = ("You are a symbolic regression expert. Analyze the data in the image and provide an improved mathematical ansatz. "
                          "Respond with ONLY the ansatz formula, without any explanation or commentary. Ensure it is in valid python. You may use numpy functions. "
                          "params is a list of parameters that can be of any length or complexity. "
