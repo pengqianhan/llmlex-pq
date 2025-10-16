3d1f44d1828e40cd2f66188f036160a0f5e87dce
pengqianhan <hanpengqian@outlook.com>
2025-10-17 10:34:35 +1300

    Enhance API test script to include image encoding and modify user query structure for image analysis

--- learningnotes/api_test_mini.py
+++ learningnotes/api_test_mini.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import numpy as np
 import matplotlib.pyplot as plt
 import llmlex
+import base64
 load_dotenv() # 加载.env文件中的环境变量
 client = openai.OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
@@ -11,12 +12,44 @@ client = openai.OpenAI(
 )
 model_name = "models/gemini-flash-lite-latest"
 
+# response = client.chat.completions.create(
+#     model=model_name,
+#     reasoning_effort="high",# default is "medium"
+#     messages=[
+#         {"role": "user", "content": "Explain how AI works in a few words"},
+#     ],
+# )
+
+# print(response.choices[0].message.content)
+
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Getting the base64 string
+base64_image = encode_image("ha_image.png")
+
 response = client.chat.completions.create(
-    model=model_name,
-    reasoning_effort="high",# default is "medium"
-    messages=[
-        {"role": "user", "content": "Explain how AI works in a few words"},
-    ],
+  model=model_name,
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What is in this image?",
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url":  f"data:image/jpeg;base64,{base64_image}"
+          },
+        },
+      ],
+    }
+  ],
 )
 
 print(response.choices[0].message.content)
